9f82b67d6a3bcda335a593d0c24a75e90a056c8d
Leonardo Viana <sonleo2013@gmail.com>
2020-07-07 13:03:18 -0400

    project with constraints

---
+++ revisao-challenge/revisao-challenge/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  revisao-challenge
+//
+//  Created by João Guilherme on 02/07/20.
+//  Copyright © 2020 João Guilherme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var texto: UILabel!
+    let butt = UIButton()
+    let textoCod = UILabel()
+    @IBOutlet var view1: UIView!
+    @IBAction func touch(_ sender: Any) {
+        texto.text = "qualquercoisa"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        
+        
+    }
+    
+
+    
+
+}
+
